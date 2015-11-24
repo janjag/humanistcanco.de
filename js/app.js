2b09e135ba384208c9f5d482ab18122c31e0bf15
Jan Jagiela <janjag@humanistcanco.de>
2015-11-24 18:54:01 +0100

    scroll mod
    + scroll history.push
    + css title/sub_title improvements

--- js/app.js
+++ js/app.js
@@ -14,6 +14,9 @@ function scroller() {
         if (cur_pos >= top && cur_pos <= bottom) {
             nav.find('li a').removeClass('active');
             
+            var hash = '#' + $(this).attr('id');
+            history.pushState(null, null, hash);
+            
             nav.find('li a[href="#' + $(this).attr('id') + '"]').addClass('active');
             if (nav.find('li a[href="#home"]').hasClass('active')) {
                 $('#left_col').find('q').hide(700, 'swing');

24d4014b2a5c4b4ca6053e82676f715e0da2d85a
janjag <janjag87@gmail.com>
2015-12-16 12:52:06 +0100

    Added new dependencies and grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,33 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        },
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('autoprefixer')({
+                        browsers: ['last 2 versions']
+                    })
+                ]
+            },
+            dist: {
+                src: 'css/*.css'
+            }
+        },
         sass: {
             options: {
                 sourceMap: true
@@ -10,11 +37,19 @@ module.exports = function(grunt) {
                     'css/main.css': 'css/partials/main.scss'
                 }
             }
+        },
+        watch: {
+            files: 'css/**/*.scss',
+            tasks: ['sass', 'postcss:dist']
         }
     });
     
+    
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-sass');
  
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
     
 }; 

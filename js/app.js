32f9cf9116ea2c0cae36cafd7703659682706ee1
janjag <janjag87@gmail.com>
2016-01-06 20:15:03 +0100

    Updated font and header slider

--- js/app.js
+++ js/app.js
@@ -14,15 +14,19 @@ function scroller() {
         if (cur_pos >= top && cur_pos <= bottom) {
             nav.find('li a').removeClass('active');
             
+            // Updating adres bar with location hash
             var hash = '#' + $(this).attr('id');
             history.pushState(null, null, hash);
             
+            //Toggling active class on navigation elements 
             nav.find('li a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            
+            //Toggling qoute block and photo/link border in header
             if (nav.find('li a[href="#about"]').hasClass('active')) {
-                $('header').find('q').hide(400, 'swing');
+                $('header').find('q').addClass('hidden').hide(600, 'swing');
                 $('header').find('.photo').addClass('active');
             } else {
-                $('header').find('q').show(400, 'swing');
+                $('header').find('q').removeClass('hidden').show(600, 'swing');
                 $('header').find('.photo').removeClass('active');
             }
         }
@@ -36,8 +40,8 @@ $(window).on('scroll', scroller);
 //test code to se if there are any changes on some devices
 $(window).on('touchmove', scroller);
 
-
-$('header').find('a').on('click', function () {
+// Scroll efect on click
+$('header').find('a').on('click', function (event) {
     event.preventDefault();
     var $el = $(this),
         id = $el.attr('href');
@@ -49,3 +53,24 @@ $('header').find('a').on('click', function () {
     });
 });
 
+// sliding header off the screen when hamburger icon is visible
+$(document).on('click', function (event) {
+    var button = $('.menu_button'),
+        header = $('.header'),
+        target = $(event.target),
+        wrapper = $('.wrapper');
+    if (button.is(':visible') && header.hasClass('header_slide') == true && target.attr('class') == 'hcc-navicon') {
+        event.stopPropagation();
+        header.toggleClass('header_slide');
+        wrapper.toggleClass('wide');
+        button.toggleClass('wide');
+        nav.find('li a[href="#about"]').toggleClass('hidden');
+    }
+    else if (button.is(':visible') && header.hasClass('header_slide') == false) {
+        event.stopPropagation();
+        header.addClass('header_slide');
+        wrapper.addClass('wide');
+        button.addClass('wide');
+        nav.find('li a[href="#about"]').removeClass('hidden');
+    }
+});
